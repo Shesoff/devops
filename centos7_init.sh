68814cffe0205795fa258ef3e38ca7e0d9a46cd4
Alexandr <shesoff@gmail.com>
2020-01-29 17:27:22 +0300

    set timezone

--- centos7_init.sh
+++ centos7_init.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+timedatectl set-timezone Europe/Moscow
 yum update -y
 yum install -y mc nmap vim sed htop telnet 
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
